7fbeff9540bbe38ea007fe01a1d0a8d790676daf
minkyu-kim <sheeeet@naver.com>
2016-12-19 16:46:25 +0900

    프로젝트 재생성 및 SummitMessage.java 수정
    MySQL DB 연동

---
+++ guestbook/src/main/java/com/basecamp/guestbook/SummitMessage.java
@@ -0,0 +1,91 @@
+package com.basecamp.guestbook;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class SummitMessage
+ */
+public class SummitMessage implements Servlet {
+	private Connection con=null;
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/guestbookdb","guestbook","rlaalsrb12");
+			System.out.println("DB connect success.");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("DB connect fail.");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String text = request.getParameter("text");
+		String query = "INSERT INTO messages (email, pass, message) VALUES (\'" +
+				email + "\', " + password + ", \'" + text + "\')";
+		Statement st;
+		int rs;
+		System.out.println(query);
+		try {
+			if(con!=null) {
+				st = con.createStatement();
+				rs = st.executeUpdate(query);
+				System.out.println(rs);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ guestbook/src/main/webapp/WEB-INF/views/guestbook.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>NHN Ent. Guestbook</title>
+</head>
+<body>
+	<form name="summitForm" method="post" action="SummitMessage">
+		<table>
+			<tr>
+				<td>이메일</td>
+				<td><input type="text" name="email" id="email" value=""/></td>
+				<td>비밀번호</td>
+				<td><input type="password" name="password" id="password" value=""/></td>
+			</tr>
+			<tr>
+				<td colspan="4">
+					본문 <br>
+					<textarea name="text" id="text" cols="50" rows="5"></textarea><br>
+					<input type="submit" value="저장" />
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
