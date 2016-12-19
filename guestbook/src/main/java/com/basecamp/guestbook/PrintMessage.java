be0aca703442e1d31c4882680cf16a262962ebbc
minkyu-kim <sheeeet@naver.com>
2016-12-19 22:41:04 +0900

    PrintMessage.java 파일 추가
    먼젓번 커밋에서 누락된 파일 추가

---
+++ guestbook/src/main/java/com/basecamp/guestbook/PrintMessage.java
@@ -0,0 +1,97 @@
+package com.basecamp.guestbook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class SummitMessage
+ */
+public class PrintMessage implements Servlet {
+	private Connection con=null;
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/guestbookdb","guestbook","rlaalsrb12");
+			System.out.println("DB connect success.");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("DB connect fail.");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		String query = "SELECT * from messages ORDER BY id DESC";
+		Statement st;
+		ResultSet rs = null;
+		String html="<table>";
+		try {
+			if(con!=null) {
+				st = con.createStatement();
+				rs = st.executeQuery(query);
+				while (rs.next()) {
+					html+="<tr>";
+					html+="<td>"+rs.getString("id")+"</td>";
+					html+="<td>"+rs.getString("email")+"</td>";
+					html+="<td>"+rs.getString("message")+"</td>";
+			        html+="</tr>";
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		html+="</table>";
+		PrintWriter out = response.getWriter();
+		out.println(html);
+	}
+}
