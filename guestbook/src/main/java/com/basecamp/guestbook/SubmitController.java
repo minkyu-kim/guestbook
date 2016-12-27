e5d57976ddcab1c72262beccdb7ed976c8794347
minkyu-kim <sheeeet@naver.com>
2016-12-27 22:00:34 +0900

    MailValidation클래스 분리 및 TC 추가

--- guestbook/src/main/java/com/basecamp/guestbook/SubmitController.java
+++ guestbook/src/main/java/com/basecamp/guestbook/SubmitController.java
@@ -10,23 +10,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class SubmitController {
 	@Autowired
 	MessageDAO mdao;
-
+	
 	@RequestMapping(value = "/submit")
 	public String execute(Model model, HttpServletRequest request) throws Exception {
 		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
-		String email = request.getParameter("email");
-		if(email==null) return "forward:/"; // http://...../submit을 직접 타이핑하여 접속한 경우
+		if(id==null) return "forward:/"; // http://...../submit을 직접 타이핑하여 접속한 경우
 		Date now = new Date(System.currentTimeMillis());
 		request.setAttribute("submitTime", now);
 		request.setAttribute("updateTime", now);
-		String regex = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
 		try {
-			if(Pattern.matches(regex, email)) {
+			if(MailValidation.check(request.getParameter("email"))) {
 				if(id=="") {
 					Message message = new MessageDAO.MessageFiller().fill(request);
 					mdao.insert(message);
--- guestbook/src/test/java/com/basecamp/guestbook/MessageDAOTest.java
+++ guestbook/src/test/java/com/basecamp/guestbook/MessageDAOTest.java
@@ -2,9 +2,9 @@ package com.basecamp.guestbook;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +22,21 @@ public class MessageDAOTest {
 	@Autowired
 	private MessageDAO mdao;
 	
+	/**		Initailized Database Status
+	 * 		+---------------------------------------------------------------------------+
+	 * 		| TABLE messages                                                            |
+	 * 		+----+----------------+--------+---------+---------------------+------------+
+	 * 		| id | email          | pass   | message | submitTime          | updateTime |
+	 * 		+----+----------------+--------+---------+---------------------+------------+
+	 * 		| 1  | test1@test.com | 123456 | test1   | 2016-12-27 16:17:53 | NULL       |
+	 * 		+----+----------------+--------+---------+---------------------+------------+
+	 * 		| 2  | test2@test.com | 234567 | test2   | 2016-12-27 16:18:53 | NULL       |
+	 * 		+----+----------------+--------+---------+---------------------+------------+
+	 * 		| 3  | test3@test.com | 345678 | test3   | 2016-12-27 16:19:53 | NULL       |
+	 *		+----+----------------+--------+---------+---------------------+------------+
+	 */		
+	
+	
 	/**
 	 * Test whether it returns exactly three messages
 	 * when MessageDAO calls SelectAll() with initialized DB.
@@ -31,5 +46,57 @@ public class MessageDAOTest {
 	public void selectAllTest() throws Exception {
 		ArrayList<Message> messages = (ArrayList<Message>) mdao.selectAll();
 		assertEquals(messages.size(),3);
+	}
+	
+	@Test
+	public void selectOneTest() throws Exception {
+		int id=1;
+		int pass=123456, wrongPass=234567;
+		Message message = new Message().setId(id).setPass(pass);
+		Message selectedMessage = mdao.selectOne(message);
+		
+		// Was selected record passed well?
+		assertEquals(selectedMessage.getId(),id);
+		assertEquals(selectedMessage.getPass(),pass);
+		assertEquals(selectedMessage.getEmail(),"test1@test.com");
+		assertEquals(selectedMessage.getMessage(),"test1");
+		assertEquals(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(selectedMessage.getSubmitTime()),"2016-12-27 16:17:53");
+		assertNull(selectedMessage.getUpdateTime());
+		
+		message.setPass(wrongPass);
+		selectedMessage = mdao.selectOne(message);
+		
+		// Is it return null value when I entered wrong password?
+		assertNull(selectedMessage);
+	}	
+	
+	/**
+	 * Test whether insertion operation has committed properly.
+	 * And it also tests whether stored record in DB has exactly same data as before.
+	 */
+	@Test
+	public void insertTest() throws Exception {
+		int id=4;
+		int pass=99999;
+		String email="insertTest@nhnent.com";
+		String text="insertTest...Hello..";
+		Date now = new Date(System.currentTimeMillis());
+		int numberOfRecordsBeforeInsert = ((ArrayList<Message>)mdao.selectAll()).size();
+		Message message = new Message().setId(id).setEmail(email).setPass(pass).
+				setMessage(text).setSubmitTime(now);
+		int insertionResult = mdao.insert(message);
+		
+		// Was insertion operation executed properly?
+		assertEquals(insertionResult,1);
+		
+		int numberOfRecordsAfterInsert = ((ArrayList<Message>)mdao.selectAll()).size();
+		
+		// Did Number of records in database increase? 
+		assertEquals(numberOfRecordsAfterInsert,numberOfRecordsBeforeInsert+1);
+		
+		Message selectedMessage = mdao.selectOne(message);
+		
+		// Was inserted data stored well?
+		assertNotNull(selectedMessage);
 	}
 }
