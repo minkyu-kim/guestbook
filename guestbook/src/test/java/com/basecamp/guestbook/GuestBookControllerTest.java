fe275fbdc6f74b4762c8f930c12466836c240f5b
minkyu-kim <sheeeet@naver.com>
2016-12-27 17:16:53 +0900

    JUnit Test 로컬에서는 JUnit Test가 패스되는데 TravisCI에서 패스가 되지않음

--- guestbook/src/test/java/com/basecamp/guestbook/GuestBookControllerTest.java
+++ guestbook/src/test/java/com/basecamp/guestbook/GuestBookControllerTest.java
@@ -13,10 +13,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
-		"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/mybatis-config.xml"})
+@WebAppConfiguration
 public class GuestBookControllerTest {
 	@Autowired
 	private GuestBookController guestbookController;
